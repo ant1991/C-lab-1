8c576362845f3c0ea56a61fa3ce10a8f2cd669ac
Denis Gandzha <gandja.denis@yandex.ru>
2018-11-27 10:25:47 +0300

    Lab 1

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,19 +1,17 @@
 ﻿#include <stdio.h>
-#include <time.h>
 #include "task2.h"
 
 
 int main()
 {
-	time_t now;
-	time(&now);
-	struct tm*t = localtime(&now);
 	
-	int tsec = t->tm_sec;
-	int tmin = t->tm_min;
-	int thour = t->tm_hour;
+	
+	
+	int tmin = 0;
+	int thour = 0;
 
-    printf("Now is %d:%d:%d\n", thour, tmin, tsec);
+    printf("Enter a time hh:mm :");
+	scanf("%d:%d", &thour, &tmin);
 	printf(greet(thour,tmin));
 	return 0;
 	
