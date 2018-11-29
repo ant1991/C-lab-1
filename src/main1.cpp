97896b6c2be63e7f8ce5e183e608a7c1906e136e
lerkas <44975578+lerkas@users.noreply.github.com>
2018-11-29 16:20:37 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include "task1.h"
 
-
-
    int main()
    {
-       float height, weight;
-       height=0;
-       weight=0;
-       char gender;
-       gender='m';
-       gender='w';
+      float height = 0;
+      float weight = 0;
+      char gender;
+      gender='m';
+      gender='w';
 
       printf("Введите ваш пол 'm' или 'w', рост в см и вес в кг: \n");
       scanf("%c, %f, %f", &gender, &height, &weight);
@@ -24,7 +21,6 @@
           else
               if(getRecommendation(gender, height, weight) == 1)
                   printf("Избыточный вес\n");
-
-
-          return 0;
+      
+      return 0;
    }
