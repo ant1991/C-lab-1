49ebae991726760324c7009f75baa2e696da4d62
AlexandrMonakhov <zabuldigaa@gmail.com>
2018-12-02 17:38:17 +0300

    Add {} in task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,6 +2,7 @@ int getRecommendation(char gender, float height, float weight)
 {
 	float ideal_weight = 0;
 	if (gender == 'm')
+	{
 		ideal_weight = height - 100;
 		if (weight < ideal_weight)
 			return -1;
@@ -9,7 +10,9 @@ int getRecommendation(char gender, float height, float weight)
 			return 1;
 		else if (weight == ideal_weight)
 			return 0;
+	}
 	else if (gender == 'w')
+	{
 		ideal_weight = height - 110;
 		if (weight < ideal_weight)
 			return -1;
@@ -17,4 +20,5 @@ int getRecommendation(char gender, float height, float weight)
 			return 1;
 		else if (weight == ideal_weight)
 			return 0;
+	}
 }
