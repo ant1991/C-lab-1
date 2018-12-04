1ad20fa21d06fb81e3146684f1728aee64bb9a79
Anton Kapralov <akapralsheikan@gmail.com>
2018-12-04 21:17:58 +0400

    removed all \n from task2.cpp

--- src/task2.cpp
+++ src/task2.cpp
@@ -3,13 +3,13 @@
 const char * greet(int hour, int min)
 {
 	if (hour < 0 || hour > 23 || min < 0 || min > 59)
-		return "Uncorrect time!\n";
+		return "Uncorrect time!";
 	if (hour >= 0 && hour < 6)
-		return "Good night!\n";
+		return "Good night!";
 	if (hour >= 6 && hour < 11)
-		return "Good morning!\n";
+		return "Good morning!";
 	if (hour >= 11 && hour < 18)
-		return "Good day!\n";
+		return "Good day!";
 	if (hour >= 18 && hour <= 23)
-		return "Good evening!\n";
+		return "Good evening!";
 }
