4d661a96820868a0a7d65fc27d7d5203df43228c
DurnevDenisR <durnev.denis.01@gmail.com>
2018-12-09 10:40:15 +0300

    lab1

---
+++ src/main2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "task2.h"
+
+void clean_stdin()
+{
+	int c;
+	do
+		c = getchar();
+	while (c != '\n'&&c != EOF);
+}
+
+
+int main()
+{
+	int hour = 0;
+	int min = 0;
+	int sec = 0;
+	while (1)
+	{
+
+		printf("Enter Hours:Minutes:Seconds:");
+		int result=scanf("%d:%d:%d", &hour, &min, &sec);
+		if (result !=3|| hour < 0 || hour >= 24 || min < 0 || min >= 60 || sec < 0 || sec >= 60)
+		{
+			printf("Incorrect time!\n");
+			clean_stdin();
+		}
+		else
+			break;
+	}
+	printf(greet(hour, min));
+
+	return 0;
+}
---
+++ src/task4.cpp
@@ -0,0 +1,9 @@
+float convert(int feet, int inches)
+
+{
+	float cm = 0;
+
+	cm = feet * 30.48 + inches * 2.54;
+
+	return cm;
+}
