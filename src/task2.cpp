db1426aa4f320512c83cc81f5673d698d31c0ce8
lerkas <44975578+lerkas@users.noreply.github.com>
2018-12-10 16:40:33 +0300

    Update task2.cpp

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,13 +1,13 @@
 const char* greet(int hour, int min)
   {
      if(hour>=00 && hour<06 && min>=00 && min<=59)
-         return "Good night!\n";
+         return "Good night!";
      else if(hour>=06 && hour<11 && min>=00 && min<=59)
-         return "Good morning!\n";
+         return "Good morning!";
      else if(hour>=11 && hour<18 && min>=00 && min<=59)
-         return "Good day!\n";
+         return "Good day!";
      else if(hour>=18 && hour<=23 && min>=00 && min<=59)
-         return "Good evening!\n";
+         return "Good evening!";
      else
-         return "Uncorrect time!\n";
+         return "Uncorrect time!";
   }
