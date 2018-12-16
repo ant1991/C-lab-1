f24286577d8d44feab1a285095586afb3bdf7d2c
IvanShapchenko <shapchenko.ivan91@mail.ru>
2018-12-16 11:39:30 +0300

    B21 lab1

---
+++ src/main4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "task4.h"
+int main()
+{
+	int feet = 0;
+	int inches = 0;
+
+	printf("Enter height(feet'inches): ");
+	scanf("%d'%d", &feet, &inches);
+	printf("%.1fcm", convert(feet, inches));
+
+	return 0;
+}
