79405f0aead114241ee9d29c768de789ba852f53
Viktoria <44978123+VinokurovaVO@users.noreply.github.com>
2019-01-12 17:45:48 +0300

    Add files via upload

---
+++ src/main4.cpp
@@ -0,0 +1,16 @@
+#include "task4.h"
+#include <stdio.h>
+
+int main()
+
+{
+	int feet = 0;
+	int inches = 0;
+	float result = 0;
+
+	printf("Vvedite dannie v formate futi'dyimi:");
+	scanf("%d'%d", &feet, &inches);
+	printf("cm=%.1f", convert(feet, inches));
+
+	return 0;
+}
